fba7810414a6adb08e9bfaeafa2b74e6f6eed4b9
Ostap <ostap024@gmail.com>
2018-09-19 15:29:19 +0300

    start

---
+++ shop-epizy.tests/shop-epizy.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("shop-epizy.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("shop-epizy.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("01a2a33a-75bf-4f87-b5ca-31035682d4f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ shop-epizy.tests/shop-epizy.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+namespace shop_epizy.tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
