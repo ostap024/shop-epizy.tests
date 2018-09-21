91605dc929adabd288b64b02fe6f9f85f3923e2b
ostap024 <40698896+ostap024@users.noreply.github.com>
2018-09-21 15:43:15 +0300

    Update TestChangePassword.cs

--- shop-epizy.tests/shop-epizy.tests/TestChangePassword.cs
+++ shop-epizy.tests/shop-epizy.tests/TestChangePassword.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Threading;
 using NUnit.Framework;
@@ -12,14 +12,14 @@ namespace shop_epizy.tests
     public class TestChangePassword
     {
         private IWebDriver driver;
+
         [OneTimeSetUp]
         public void BeforeAllMethods()
         {
             driver = new ChromeDriver();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            
-
         }
+
         [Test]
         public void Tets()
         {
@@ -34,6 +34,16 @@ namespace shop_epizy.tests
             LogOut();
         }
 
+        [TearDown]
+        public void TearDown()
+        {
+            string email = "Ostap@gmail.com";
+            string currPassword = "q1w2e3r4t5y6";
+            string newPassword = "qwerty123";
+            LogOut();
+            LogIn(email, currPassword);
+            LogOut();
+        }
         private void LogIn(string email, string password)
         {
             driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/index.php?route=account/login");
@@ -67,29 +77,22 @@ namespace shop_epizy.tests
             driver.FindElement(By.CssSelector("a.btn.btn-primary")).Click();
             Thread.Sleep(2000);
         }
-        private void ChangePass1(string newPassword)
-        {
-            driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/index.php?route=account/password");
 
-            driver.FindElement(By.Id("input-password")).SendKeys(newPassword);
-
-            driver.FindElement(By.Id("input-confirm")).SendKeys(newPassword);
-
-            driver.FindElement(By.CssSelector("input.btn.btn-primary")).Click();
-            Thread.Sleep(2000);
-        }
 
         [OneTimeTearDown]
         public void AfterAllMethods()
         {
-            string email = "Ostap@gmail.com";
-            string currPassword = "q1w2e3r4t5y6";
-            string newPassword = "qwerty123";
-            LogOut();
-            LogIn(email, currPassword);
-            ChangePass1(newPassword);
-            LogOut();
+            
             driver.Quit();
         }
+        /*
+         "#button-search + h2 + p"
+"//input[@id='button-search']/following-sibling::p"
+"//input[@id='button-search']/following-sibling::p[contains(text(),'matches')]"
+         змінні в датапровайдер
+         патерн стратегія
+         "//div[contains(@class, 'product-layout')]//a[contains(text(),'Apple Cinema')]/../../following-sibling::div/button[contains(@data-original-title,'Wish')]"
+
+         */
     }
 }
