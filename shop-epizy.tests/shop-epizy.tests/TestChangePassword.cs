aad58bcfc037f1798c7a8e16f9bc4fda269c1013
Ostap <ostap024@gmail.com>
2018-09-22 19:22:39 +0300

    sfefs

--- shop-epizy.tests/shop-epizy.tests/TestChangePassword.cs
+++ shop-epizy.tests/shop-epizy.tests/TestChangePassword.cs
@@ -20,6 +20,12 @@ namespace shop_epizy.tests
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
 
+        [SetUp]
+        public void SetUp()
+        {
+            driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/");
+        }
+
         [Test]
         public void Tets()
         {
@@ -27,68 +33,106 @@ namespace shop_epizy.tests
             string email = "Ostap@gmail.com";
             string currPassword = "qwerty123";
             string newPassword = "q1w2e3r4t5y6";
-
+            string changePassExpected = "Success: Your password has been successfully updated.";
+            string logInExpected = "Login success";
             //Act
-            LogIn(email, currPassword);
-            ChangePass(newPassword);
+            string logInActual = LogIn(email, currPassword);
+            Assert.AreEqual(logInExpected, logInActual, logInActual);//робити в кінці чи після кожного виклику
+
+
+            string changePassActual = ChangePass(newPassword);
+            Assert.AreEqual(changePassExpected, changePassActual);
+
             LogOut();
+
+            string secondLogInActual = LogIn(email, newPassword);
+            Assert.AreEqual(logInExpected, secondLogInActual, secondLogInActual);//робити в кінці чи після кожного виклику
+
         }
 
-        [TearDown]
-        public void TearDown()
+        private string LogIn(string email, string password)
         {
-            string email = "Ostap@gmail.com";
-            string currPassword = "q1w2e3r4t5y6";
-            string newPassword = "qwerty123";
-            LogOut();
-            LogIn(email, currPassword);
-            LogOut();
-        }
-        private void LogIn(string email, string password)
-        {
-            driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/index.php?route=account/login");
+            driver.FindElement(By.XPath("//a[@title='My Account']")).Click();
+            driver.FindElement(By.XPath("//ul[@class='dropdown-menu dropdown-menu-right']//a['login' = substring(@href, string-length(@href) - string-length('login') +1)]")).Click();
 
             driver.FindElement(By.Id("input-email")).SendKeys(email);
 
             driver.FindElement(By.Id("input-password")).SendKeys(password);
             driver.FindElement(By.CssSelector("input.btn.btn-primary")).Click();
-            //  Thread.Sleep(2000);
+
+            driver.FindElement(By.XPath("//a[@title='My Account']")).Click();//open dropdown menu
+            if (IsElementPresent(By.XPath("//ul[@class='dropdown-menu dropdown-menu-right']//a['account' = substring(@href, string-length(@href) - string-length('account') +1)]")))
+            {
+                driver.FindElement(By.XPath("//a[@title='My Account']")).Click();//close dropdown menu
+                return "Login success";
+            }
+            else
+            {
+                driver.FindElement(By.XPath("//a[@title='My Account']")).Click();//close dropdown menu
+                return "Login fail";
+            }
+           
+
         }
 
-        private void ChangePass(string newPassword)
-        {
-            string expected = "Success: Your password has been successfully updated.";
-            //Debug.WriteLine("ddd");
-            driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/index.php?route=account/password");
 
+        private string ChangePass(string newPassword)
+        {
+
+            driver.FindElement(By.XPath("//a[@title='My Account']")).Click();
+            driver.FindElement(By.XPath("//ul[@class='dropdown-menu dropdown-menu-right']//a['account' = substring(@href, string-length(@href) - string-length('account') +1)]")).Click();
+            driver.FindElement(By.XPath("//div[@id='content']//a['password' = substring(@href, string-length(@href) - string-length('password') +1)]")).Click();
             driver.FindElement(By.Id("input-password")).SendKeys(newPassword);
 
             driver.FindElement(By.Id("input-confirm")).SendKeys(newPassword);
 
             driver.FindElement(By.CssSelector("input.btn.btn-primary")).Click();
-            string actual = driver.FindElement(By.CssSelector("div.alert.alert-success")).Text;
-            Assert.AreEqual(expected, actual);
-            Thread.Sleep(2000);
+            return driver.FindElement(By.CssSelector("div.alert.alert-success")).Text;
         }
 
         private void LogOut()
         {
+            driver.FindElement(By.XPath("//a[@title='My Account']")).Click();
+            driver.FindElement(By.XPath("//ul[@class='dropdown-menu dropdown-menu-right']//a['logout' = substring(@href, string-length(@href) - string-length('logout') +1)]")).Click();
+
+            driver.FindElement(By.CssSelector("a.btn.btn-primary")).Click();
+            // Thread.Sleep(2000);
+        }
+        private void LogOutURL()
+        {
             driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/index.php?route=account/logout");
             driver.FindElement(By.CssSelector("a.btn.btn-primary")).Click();
-           // Thread.Sleep(2000);
+            Thread.Sleep(2000);
         }
-
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
 
         [OneTimeTearDown]
         public void AfterAllMethods()
         {
-            
+            string email = "Ostap@gmail.com";
+            string currPassword = "q1w2e3r4t5y6";
+            string newPassword = "qwerty123";
+            LogOutURL();
+            LogIn(email, currPassword);
+            ChangePass(newPassword);
+            LogOut();
             driver.Quit();
         }
         /*
          "#button-search + h2 + p"
-"//input[@id='button-search']/following-sibling::p"
-"//input[@id='button-search']/following-sibling::p[contains(text(),'matches')]"
+    "//input[@id='button-search']/following-sibling::p"
+    "//input[@id='button-search']/following-sibling::p[contains(text(),'matches')]"
          змінні в датапровайдер
          патерн стратегія
          "//div[contains(@class, 'product-layout')]//a[contains(text(),'Apple Cinema')]/../../following-sibling::div/button[contains(@data-original-title,'Wish')]"
