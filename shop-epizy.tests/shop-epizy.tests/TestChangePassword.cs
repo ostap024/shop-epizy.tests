1aa99609b2791b5d793517494770a8691c54e0b8
ostap024 <40698896+ostap024@users.noreply.github.com>
2018-09-24 12:09:25 +0300

    Update TestChangePassword.cs

--- shop-epizy.tests/shop-epizy.tests/TestChangePassword.cs
+++ shop-epizy.tests/shop-epizy.tests/TestChangePassword.cs
@@ -5,9 +5,6 @@ using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using LumenWorks.Framework.IO.Csv;
-using System.IO;
-using System.Collections;
 
 namespace shop_epizy.tests
 {
@@ -26,13 +23,6 @@ namespace shop_epizy.tests
         [OneTimeTearDown]
         public void AfterAllMethods()
         {
-            string email = "Ostap@gmail.com";
-            string currPassword = "q1w2e3r4t5y6";
-            string newPassword = "qwerty123";
-            LogOutURL();
-            LogIn(email, currPassword);
-            ChangePass(newPassword);
-            LogOut();
             driver.Quit();
         }
 
@@ -43,12 +33,10 @@ namespace shop_epizy.tests
             //Arrange
             string changePassExpected = "Success: Your password has been successfully updated.";
             string logInExpected = "Login success";
+
             //Act
-
-
             string logInActual = LogIn(email, currPassword);
             Assert.AreEqual(logInExpected, logInActual, logInActual);//робити в кінці чи після кожного виклику
-
 
             string changePassActual = ChangePass(newPassword);
             Assert.AreEqual(changePassExpected, changePassActual);
@@ -58,6 +46,10 @@ namespace shop_epizy.tests
             string secondLogInActual = LogIn(email, newPassword);
             Assert.AreEqual(logInExpected, secondLogInActual, secondLogInActual);//робити в кінці чи після кожного виклику
 
+            LogOut();
+            LogIn(email, newPassword);
+            ChangePass(currPassword);
+            LogOut();
         }
 
         private string LogIn(string email, string password)
@@ -80,16 +72,13 @@ namespace shop_epizy.tests
             {
                 driver.FindElement(By.XPath("//a[@title='My Account']")).Click();//close dropdown menu
                 return "Login fail";
-            }
-           
-
+            }      
         }
 
 
         private string ChangePass(string newPassword)
         {
-
-            driver.FindElement(By.XPath("//a[@title='My Account']")).Click();
+            driver.FindElement(By.XPath("//a[@title='My Account']")).Click();//open dropdown menu
             driver.FindElement(By.XPath("//ul[@class='dropdown-menu dropdown-menu-right']//a['account' = substring(@href, string-length(@href) - string-length('account') +1)]")).Click();
             driver.FindElement(By.XPath("//div[@id='content']//a['password' = substring(@href, string-length(@href) - string-length('password') +1)]")).Click();
             driver.FindElement(By.Id("input-password")).SendKeys(newPassword);
@@ -106,32 +95,9 @@ namespace shop_epizy.tests
             driver.FindElement(By.XPath("//ul[@class='dropdown-menu dropdown-menu-right']//a['logout' = substring(@href, string-length(@href) - string-length('logout') +1)]")).Click();
 
             driver.FindElement(By.CssSelector("a.btn.btn-primary")).Click();
-            // Thread.Sleep(2000);
         }
 
-        private void LogOutURL()
-        {
-            driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/index.php?route=account/logout");
-            driver.FindElement(By.CssSelector("a.btn.btn-primary")).Click();
-            Thread.Sleep(2000);
-        }
-        
-        public  static IEnumerable GetTestData
-        {
-            get
-            {
-                using (CsvReader csv = new CsvReader(new StreamReader(TestContext.CurrentContext.TestDirectory + @"\test-data.csv"), true))
-                {
-                    while (csv.ReadNextRecord())
-                    {
-                        string email = csv[0];
-                        string currPassword = csv[1];
-                        string newPassword = csv[2];
-                        yield return new TestCaseData(email, currPassword, newPassword);
-                    }
-                }
-            }
-        }
+
 
         private bool IsElementPresent(By by)
         {
